b6cb9e3ae116e9e84425c98d6fdc66f55838d80a
Lim Sergei <si.limitted@gmail.com>
2021-04-23 13:16:48 +0300

    feat: закончить popup

--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,27 @@ const openPopupButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const closePopupButton = document.querySelector('.popup__close-button');
 
-function togglePopup(event) {
+let profileName = document.querySelector('.profile__name');
+let profileActivity = document.querySelector('.profile__activity');
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__input_name');
+let jobInput = document.querySelector('.popup__input_activity');
+
+function togglePopup() {
   popup.classList.toggle('popup_opened');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileActivity.textContent;
+}
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileActivity.textContent = jobInput.value;
+  togglePopup();
 }
 
 openPopupButton.addEventListener('click', togglePopup);
 closePopupButton.addEventListener('click', togglePopup);
+formElement.addEventListener('submit', formSubmitHandler);
+
 
